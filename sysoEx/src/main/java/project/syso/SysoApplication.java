2a202ec7ddb3c4e65da9ab41551d31591ff0caa6
oyfkr <oyfkr@naver.com>
2021-10-28 23:35:04 +0900

    프로젝트 예시 디펜던시 추가 스프링 버젼

---
+++ sysoEx/src/main/java/project/syso/SysoApplication.java
@@ -0,0 +1,13 @@
+package project.syso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysoApplication.class, args);
+    }
+
+}
---
+++ sysoEx/src/test/java/project/syso/SysoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.syso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
